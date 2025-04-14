b399988fd5ff6d43c3513a83fa6ce43ec5997fe5
Leo Choi <leochoi@fseng.com.hk>
2025-04-14 11:23:37 +0800

    dev

---
+++ lib/components/modal/modal.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learning/theme/AppTheme.dart';
+
+class Modal {
+  double? height;
+  Widget content;
+
+  Modal(this.height, this.content);
+}
